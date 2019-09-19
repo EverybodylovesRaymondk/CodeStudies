363e9aa7439f7bd65b3f4bd3b7ea603e6a44c194
Raymond Kriel <raymondk@spsi.co.za>
2019-09-19 23:35:05 +0200

    Java Course

---
+++ Java/Hello World/src/Hello.java
@@ -0,0 +1,14 @@
+public class Hello {
+
+    public static void main(String[] args){
+        System.out.println("Hello, Raymond");
+
+        int myFirstNumber = (10+5)+(2*10);
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+        int myTotal= myFirstNumber + mySecondNumber + myThirdNumber;
+        System.out.println("The Total : "+ myTotal);
+        int myLastOne= 1000- myTotal;
+        System.out.println("Last Total : "+myLastOne);
+    }
+}
