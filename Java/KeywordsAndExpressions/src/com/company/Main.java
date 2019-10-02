c1b234bb6735decc72d5599452bcd1d4081affea
Raymond Kriel <raymondk@spsi.co.za>
2019-10-02 17:13:16 +0200

    Java Code study 10/02/2019

---
+++ Java/KeywordsAndExpressions/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //Video 43
+        //a mile is equal to 1.609344 kilometers
+        double kilometers=(100*1.609344);
+        int highscore=50;
+
+          if (highscore==50){
+            System.out.println("This is an expression");
+        }
+    }
+}
