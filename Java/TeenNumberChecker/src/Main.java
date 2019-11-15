79a8c05c474d3f94eb62cd98eeb51484b58c6954
Raymond Kriel <raymondk@spsi.co.za>
2019-11-15 18:43:33 +0200

    Code studies 15-11-19

---
+++ Java/TeenNumberChecker/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(TeenNumberChecker.hasTeen(12,12,15));
+        System.out.println(TeenNumberChecker.isTeen(20));
+
+    }
+}
