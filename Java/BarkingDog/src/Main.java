6656231b5486640dced9a5e1c995c595b17d394d
Raymond Kriel <raymondk@spsi.co.za>
2019-11-27 15:46:48 +0200

    CODE STUDIES 27/11/2019

--- Java/BarkingDog/src/Main.java
+++ Java/BarkingDog/src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Barkingdog.shouldWakeUp(true,-2));
+        System.out.println(Barkingdog.shouldWakeUp(true,23));
     }
 }
