ba176050f2f588252c46afff88566ef6dfa42cd0
Raymond Kriel <raymondk@spsi.co.za>
2019-12-17 20:11:36 +0200

    code studies 17/12/2019

--- Java/PlayingCat/src/PlayingCat.java
+++ Java/PlayingCat/src/PlayingCat.java
@@ -1,5 +1,11 @@
 public class PlayingCat {
     public static boolean isCatPlaying(boolean summer,int temperature){
-     return false;
+        int lowerLimit = 25;
+        int upperLimit = summer ? 45 : 35; //The "?" is for the value of summer if it summer is yes then it is 45 if no then it is 35
+
+        if (temperature >= lowerLimit && temperature <= upperLimit) {
+            return true;
+        }
+        return false;
     }
 }
