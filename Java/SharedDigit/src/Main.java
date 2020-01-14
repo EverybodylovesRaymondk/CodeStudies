1463b29a49d53ac6c2a9cf4d3bfa15e9220b334d
Raymond Kriel <raymondk@spsi.co.za>
2020-01-14 19:31:21 +0200

    Code studies 14/01/2020

--- Java/SharedDigit/src/Main.java
+++ Java/SharedDigit/src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(SharedDigit.SharedDigit(10,9));
+        System.out.println(SharedDigit.hasSharedDigit(9,99));
     }
 }
