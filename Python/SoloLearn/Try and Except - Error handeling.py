969c8fb7e28fc2c35a845013cefbe8e1145180ad
Raymond Kriel <raymondk@spsi.co.za>
2020-02-14 15:43:07 +0200

    Code studies 14/02/2020

--- Python/SoloLearn/Try and Except - Error handeling.py
+++ Python/SoloLearn/Try and Except - Error handeling.py
@@ -2,7 +2,7 @@ try:
     num1=7
     num2=0
     print(num1/num2)
-    print("Done calculation")
+   # print("Done calculation")
 except ZeroDivisionError:
-    print("An error occurred")
-    print("due to zeero division")
+   print("An error occurred")
+   print("due to zeero division")
