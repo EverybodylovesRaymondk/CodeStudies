32112acf7cb232f64ce7bbe5420b6fd6069c1263
Raymond Kriel <raymondk@icetech.io>
2021-01-10 18:21:58 +0200

    code studies Automate the boring things 10/01/2020

---
+++ Python/Automate the boring stuff/Section 16 GUI Automation/Controling mouse and keyboard.py
@@ -0,0 +1,30 @@
+import pyautogui
+import time
+
+print(pyautogui.size())
+time.sleep(3)
+
+
+print(pyautogui.position())
+pyautogui.moveTo(406,31)
+pyautogui.click()
+
+pyautogui.moveTo(424,58)
+pyautogui.click()
+
+#closing the preferences.
+pyautogui.moveTo(1455,712,1.5)
+pyautogui.click()
+
+
+#Opening menue with keyboard shortcuts.
+pyautogui.hotkey('alt','t')
+time.sleep(2)
+pyautogui.press('escape')
+pyautogui.press('escape')
+
+
+#NOTE: To get the co ordinates of an object start the cmd terminal with python
+#import the pyautogui module
+#run pyautoqui.displayMousePosition()
+# press ctrl + C to exit.
